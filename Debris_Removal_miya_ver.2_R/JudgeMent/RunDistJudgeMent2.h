8504661d3f8d980fc5ae7e3b38a6ea7578aa5cb9
hamapoly4 <168153463+hamapoly4@users.noreply.github.com>
2024-10-02 17:46:58 +0900

    Add files via upload

---
+++ Debris_Removal_miya_ver.2_R/JudgeMent/RunDistJudgeMent2.h
@@ -0,0 +1,26 @@
+﻿#ifndef ___CLASS_RUNDISTJUDGEMENT2
+#define ___CLASS_RUNDISTJUDGEMENT2
+
+#include "JudgeMent.h"
+#include "RunDistDetect.h"
+
+class RunDistJudgeMent2 : public JudgeMent {
+private:
+	RunDistDetect *mRunDistDetect1;
+	RunDistDetect *mRunDistDetect2;
+
+	enum eTire { RIGHT, LEFT,};
+
+	float mtarget_dist1;
+	float mtarget_dist2;
+	float mcurrent_dist1;
+	float mcurrent_dist2;
+	unsigned char mtire;
+	
+public:
+	RunDistJudgeMent2(unsigned char tire, float target_dist);
+	~RunDistJudgeMent2();
+	bool judge();
+};
+
+#endif // ___CLASS_RUNDISTJUDGEMENT
---
+++ Debris_Removal_miya_ver.2_R/Scenario/Scene.cpp
@@ -0,0 +1,130 @@
+﻿#include "Scene.h"
+#include <stdio.h>
+
+Scene::Scene(unsigned char scene_no)
+	: mcurrent_scene_no(scene_no)
+{
+	// 各シーンに応じた走行オブジェクトを確保
+	switch (mcurrent_scene_no)
+	{
+		case 0:
+		case 6:
+		case 12:
+		case 18:
+		case 24:
+		case 30:
+		case 36:
+		case 42:
+		case 48:
+		case 54:
+			mRun[mcurrent_scene_no] = new StraRun(50);
+			break;
+			
+		case 2:
+		case 8:
+		case 14:
+		case 20:
+		case 26:
+		case 32:
+		case 38:
+		case 44:
+		case 50:
+			mRun[mcurrent_scene_no] = new StraRun(-50);
+			break;
+			
+		case 4:
+		case 22:
+		case 28:
+		case 34:
+		case 52:
+			mRun[mcurrent_scene_no] = new RotateRun(LEFT);
+			break;
+			
+		case 10:
+		case 16:
+		case 40:
+		case 46:
+			mRun[mcurrent_scene_no] = new RotateRun(RIGHT);
+			break;
+			
+		default:
+			for(i=0;i<150000;i++){
+				mRun[mcurrent_scene_no] = new StraRun(0);
+			}
+			break;
+	}
+
+	// 各シーンに応じた判定オブジェクトを確保
+	switch (mcurrent_scene_no)
+	{
+		case 0:
+			mJudgeMent[mcurrent_scene_no] = new RunDistJudgeMent(BOTH, 140);	//start
+			break;
+		case 12:
+		case 30:
+		case 36:
+		case 42:
+		case 48:
+			mJudgeMent[mcurrent_scene_no] = new RunDistJudgeMent(BOTH, 395);	//1block	block*360+35
+			break;
+		case 24:
+			mJudgeMent[mcurrent_scene_no] = new RunDistJudgeMent(BOTH, 755);	//2block
+			break;
+		case 6:
+		case 18:
+			mJudgeMent[mcurrent_scene_no] = new RunDistJudgeMent(BOTH, 1115);	//3block
+			break;
+		case 54:
+			mJudgeMent[mcurrent_scene_no] = new RunDistJudgeMent(BOTH, 360);	//end
+			break;
+			
+		case 2:
+		case 8:
+		case 14:
+		case 20:
+		case 26:
+		case 32:
+		case 38:
+		case 44:
+		case 50:
+			mJudgeMent[mcurrent_scene_no] = new RunDistJudgeMent(BOTH, -50);	//back
+			break;
+			
+		case 4:
+		case 22:
+		case 28:
+		case 34:
+		case 52:
+			mJudgeMent[mcurrent_scene_no] = new RunDistJudgeMent2(LEFT, 76);
+			break;
+		
+		case 10:
+		case 16:
+		case 40:
+		case 46:
+			mJudgeMent[mcurrent_scene_no] = new RunDistJudgeMent2(RIGHT, 74);
+			break;
+		
+		default:
+			mJudgeMent[mcurrent_scene_no] = new RunDistJudgeMent(BOTH, 0);
+			break;
+	}
+}
+
+Scene::~Scene()
+{
+	delete mRun[mcurrent_scene_no];
+	delete mJudgeMent[mcurrent_scene_no];
+
+	printf("シーン番号：%d シーンオブジェクトの解放！！\n\n", mcurrent_scene_no + 1);
+}
+
+void Scene::run()
+{
+	mRun[mcurrent_scene_no]->run();
+}
+
+bool Scene::fin_judge()
+{
+	return mJudgeMent[mcurrent_scene_no]->judge();
+}
